a6ef05145177b6a4cd8e0093a7ff7c67efd9e577
sunilkmr210 <87411181+sunilkmr210@users.noreply.github.com>
2023-08-23 15:16:28 +0530

    Update Minimax.h

--- Minimax.h
+++ Minimax.h
@@ -1,11 +1,3 @@
-/**
- * Tyler Thompson
- * Intro Into Artificial Intelligence
- *
- * The purpose of this project is to use the Minimax Algorithm
- * in order to play against the computer, or you may play against
- * another human and test your skills.
- */
 
 #ifndef MINIMAX_H
 #define MINIMAX_H
